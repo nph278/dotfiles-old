c1efe121265e444cf5cd66d2eacf166659a4c2be
nph <carllegrone@protonmail.com>
2021-05-24 10:30:29 -0400

    waybar install

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ cp .zshrc ~/.zshrc
 cp -r .config/wallpaper ~/.config/wallpaper
 cp -r .config/alacritty ~/.config/alacritty
 cp -r .config/sway ~/.config/sway
+cp -r .config/waybar ~/.config/waybar
 rm -rf .SpaceVim.d
 cp -r .SpaceVim.d ~/.SpaceVim.d
