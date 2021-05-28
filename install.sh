156047688022e23d757afc465d5d16aba7ff6ad2
nph <carllegrone@protonmail.com>
2021-05-28 15:00:58 -0400

    No more wallpaper

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 curl -sLf https://spacevim.org/install.sh | bash
 cp .zshrc ~/.zshrc
-cp .config/wallpaper.png ~/.config/wallpaper.png
 cp -r .config/alacritty ~/.config/alacritty
 cp -r .config/sway ~/.config/sway
 cp -r .config/waybar ~/.config/waybar
