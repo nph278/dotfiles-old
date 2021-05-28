14bb627f3cf1610a05f1eac990a475439399d579
nph <carllegrone@protonmail.com>
2021-05-28 14:37:07 -0400

    htop + bpytop

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ cp .config/wallpaper.png ~/.config/wallpaper.png
 cp -r .config/alacritty ~/.config/alacritty
 cp -r .config/sway ~/.config/sway
 cp -r .config/waybar ~/.config/waybar
+cp -r .config/htop ~/.config/bpytop
+cp -r .config/bpytop ~/.config/bpytop
 rm -rf ~/.SpaceVim.d
 cp -r .SpaceVim.d ~/.SpaceVim.d
