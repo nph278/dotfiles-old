4dceac2edaf1fb8330b72a1e6d2997a278c95073
nph <carllegrone@protonmail.com>
2021-05-28 14:25:12 -0400

    fix very broken install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 curl -sLf https://spacevim.org/install.sh | bash
 cp .zshrc ~/.zshrc
-cp -r .config/wallpaper ~/.config/wallpaper
+cp .config/wallpaper.png ~/.config/wallpaper.png
 cp -r .config/alacritty ~/.config/alacritty
 cp -r .config/sway ~/.config/sway
 cp -r .config/waybar ~/.config/waybar
-rm -rf .SpaceVim.d
+rm -rf ~/.SpaceVim.d
 cp -r .SpaceVim.d ~/.SpaceVim.d
