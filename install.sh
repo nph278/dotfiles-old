1a43d487318d5eca23ed2523353f431889a34a3f
nph <carllegrone@protonmail.com>
2021-05-29 05:51:17 -0400

    better installer

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 cp .zshrc ~/.zshrc
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/sway
+rm -rf ~/.config/waybar
+rm -rf ~/.config/htop
+rm -rf ~/.config/bpytop
 rm -rf ~/.config/nvim
+rm -rf ~/.weechat
 cp -r .config/alacritty ~/.config/alacritty
 cp -r .config/sway ~/.config/sway
 cp -r .config/waybar ~/.config/waybar
