313fa97b66e9e317ad7fc261a88a161721dae990
nph <carllegrone@protonmail.com>
2021-05-29 08:44:19 -0400

    tmux conf

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ rm -rf ~/.config/htop
 rm -rf ~/.config/bpytop
 rm -rf ~/.config/nvim
 rm -rf ~/.weechat
+rm ~/.tmux.conf
 cp -r .config/alacritty ~/.config/alacritty
 cp -r .config/sway ~/.config/sway
 cp -r .config/waybar ~/.config/waybar
@@ -17,3 +18,4 @@ cp -r .config/htop ~/.config/bpytop
 cp -r .config/bpytop ~/.config/bpytop
 cp -r .config/nvim ~/.config/nvim
 cp -r .weechat ~/.weechat
+cp .tmux.conf ~/.tmux.conf
