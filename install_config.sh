421ef0534bf70f8be944f21f93d5ec3604273b59
nph278 <carllegrone@protonmail.com>
2021-06-24 16:10:42 -0400

    clean up

--- install_config.sh
+++ install_config.sh
@@ -1,11 +1,10 @@
 # Installs dotfiles
 
-cp .zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions --depth=1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting --depth=1
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir ~/.config
+mkdir -p ~/.config
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/sway
 rm -rf ~/.config/waybar
@@ -14,13 +13,14 @@ rm -rf ~/.config/bpytop
 rm -rf ~/.config/nvim
 rm -rf ~/.config/qutebrowser
 rm -rf ~/.weechat
-rm ~/.tmux.conf
-cp -r .config/alacritty ~/.config/alacritty
-cp -r .config/sway ~/.config/sway
-cp -r .config/waybar ~/.config/waybar
-cp -r .config/htop ~/.config/bpytop
-cp -r .config/bpytop ~/.config/bpytop
-cp -r .config/nvim ~/.config/nvim
-cp -r .config/qutebrowser ~/.config/qutebrowser
-cp -r .weechat ~/.weechat
-cp .tmux.conf ~/.tmux.conf
+rm -f ~/.tmux.conf
+cp -r config/alacritty ~/.config/alacritty
+cp -r config/sway ~/.config/sway
+cp -r config/waybar ~/.config/waybar
+cp -r config/htop ~/.config/bpytop
+cp -r config/bpytop ~/.config/bpytop
+cp -r config/nvim ~/.config/nvim
+cp -r config/qutebrowser ~/.config/qutebrowser
+cp -r config/weechat ~/.weechat
+cp config/.tmux.conf ~/.tmux.conf
+cp config/.zshrc ~/.zshrc
