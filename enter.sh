d599b05b47dd7c9c501378e6d798da46198d47e9
nph278 <carllegrone@protonmail.com>
2021-07-28 16:33:58 -0400

    goodbye nix

--- enter.sh
+++
@@ -1,8 +0,0 @@
-passwd carl
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/carl/.zsh/zsh-autosuggestions --depth=1
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/carl/.zsh/zsh-syntax-highlighting --depth=1
-curl -fLo /home/carl/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cd /home/carl/Projects/dotfiles
-./install-config.sh
-
