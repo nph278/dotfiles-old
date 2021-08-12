22729c21f0a1c2257206e076691cda844c1259eb
nph278 <carllegrone@protonmail.com>
2021-08-12 10:43:03 -0400

    doesnt work yet

--- enter.sh
+++ enter.sh
@@ -1,7 +1,7 @@
 passwd carl
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/carl/.zsh/zsh-autosuggestions --depth=1
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/carl/.zsh/zsh-syntax-highlighting --depth=1
-curl -fLo /home/carl/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/carl/.zsh/zsh-autosuggestions --depth=1
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/carl/.zsh/zsh-syntax-highlighting --depth=1
+#curl -fLo /home/carl/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd /home/carl/Projects/dotfiles
 stow alacritty sway zsh nvim dash
